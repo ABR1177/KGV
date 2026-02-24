ac34d328b39ff34584b2a2d64c7fb65c651d8020
ABR1177 <alexbrehm@web.de>
2026-02-24 23:52:11 +0100

    Add files via upload
    Korrekturen

--- sw.js
+++ sw.js
@@ -1,19 +1,29 @@
-const CACHE_NAME = 'kgv-manager-v1';
+const CACHE_NAME = 'birthday-cache-v2';
 const ASSETS = [
-    'index.html',
-    'app.js',
-    'style.css',
-    'manifest.json'
+  './',
+  './index.html',
+  './style.css',
+  './animations.css',
+  './app.js',
+  './manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
